e345b93a7dda9b4443db40758365ba337f3f1036
pawarshivamd <shivam.p@konzeptsolutions.com>
2023-06-13 19:32:46 +0530

    --last

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
-        document.getElementById("loginForm").addEventListener("submit", function (event) {
-            event.preventDefault(); // Prevent form submission
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
 
-            // Get username and password values
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
+    // Get username and password values
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-            // Check if username or password is empty
-            if (username === "" || password === "") {
-                // Display error message for empty values
-                document.getElementById("error").style.display = "none";
-                document.getElementById("emptyError").style.display = "block";
-            } else if (username === "admin@afrg.com" && password === "123456") {
-                // Perform successful login action
-                window.location.href = "Documents-page.html";
-            } else {
-                // Display error message for invalid credentials
-                document.getElementById("emptyError").style.display = "none";
-                document.getElementById("error").style.display = "block";
-            }
-        });
+    // Check if username or password is empty
+    if (username === "" || password === "") {
+        // Display error message for empty values
+        document.getElementById("error").style.display = "none";
+        document.getElementById("emptyError").style.display = "block";
+    } else if (username === "admin@afrg.com" && password === "123456") {
+        // Perform successful login action
+        window.location.href = "Documents-page.html";
+    } else {
+        // Display error message for invalid credentials
+        document.getElementById("emptyError").style.display = "none";
+        document.getElementById("error").style.display = "block";
+    }
+});
